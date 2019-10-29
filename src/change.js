34fc12a0dfebd772609900ce6e2e854e1e3b4036
Ellis Halliday <1478pop@gmail.com>
2019-10-29 12:57:23 -0400

    Just getting started

--- src/change.js
+++ src/change.js
@@ -6,9 +6,9 @@ const MAX = 0.99;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid")// print your error message here
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid")// print your error message here
 } else {
     // write your code for making change here
 }
